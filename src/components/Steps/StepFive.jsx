929043c17e45f3222da6811b0fce5efed6a4c77e
developerjay18 <jayrai3003@gmail.com>
2024-10-18 10:59:20 +0530

    update once again

--- src/components/Steps/StepFive.jsx
+++ src/components/Steps/StepFive.jsx
@@ -30,7 +30,7 @@ const StepFive = ({ nextStep, prevStep }) => {
                   <span>Service Explanation PDF</span>
                 </Link>
               </button>
-              <button className="bg-black rounded-full px-4 lg:px-6 py-2 lg:py-3 text-white text-lg w-[250px]">
+              <button className="bg-black rounded-full px-4 lg:px-6 py-2 lg:py-3 text-white text-lg lg:w-[250px]">
                 <Link to={'https://maps.app.goo.gl/uxjxNktAQbPvkSK66'}>
                   <span></span>
                   <span>Read Our Reviews</span>
